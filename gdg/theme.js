872b76b61c9f2c67a2a5cbc51dbc48b4d779b9ca
Shafaen <sheikh.shafaenali2025@vitstudent.ac.in>
2026-01-22 15:36:55 +0530

    Update theme.js

--- gdg/theme.js
+++ gdg/theme.js
@@ -1,11 +1,10 @@
 const toggleTheme = () => {
   document.body.classList.toggle("dark");
-  localStorage.setItem(
-    "theme",
-    document.body.classList.contains("dark")
-  );
+  localStorage.setItem("theme", document.body.classList.contains("dark"));
 };
 
 if (localStorage.getItem("theme") === "true") {
   document.body.classList.add("dark");
 }
+
+
